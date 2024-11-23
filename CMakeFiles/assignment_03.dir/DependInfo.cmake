b4404ea261a7e2697d4fbcebafc1d6ef9daa884a
julian <Julian.Frei@student.uibk.ac.at>
2024-11-24 00:13:13 +0100

    Added functionality to change the plane's nose to the direction of motion. Also Plane_belly is facing towards but still need to fix to cirle on yz-plane (currently xz-plane)

---
+++ CMakeFiles/assignment_03.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/julian/uni/visual/PS/Assignment_3/src/assignment_3.cpp" "CMakeFiles/assignment_03.dir/src/assignment_3.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/assignment_3.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/flag.cpp" "CMakeFiles/assignment_03.dir/src/flag.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/flag.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/math/matrix3d.cpp" "CMakeFiles/assignment_03.dir/src/math/matrix3d.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/math/matrix3d.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/math/matrix4d.cpp" "CMakeFiles/assignment_03.dir/src/math/matrix4d.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/math/matrix4d.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/math/vector2d.cpp" "CMakeFiles/assignment_03.dir/src/math/vector2d.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/math/vector2d.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/math/vector3d.cpp" "CMakeFiles/assignment_03.dir/src/math/vector3d.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/math/vector3d.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/math/vector4d.cpp" "CMakeFiles/assignment_03.dir/src/math/vector4d.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/math/vector4d.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/mygl/base.cpp" "CMakeFiles/assignment_03.dir/src/mygl/base.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/mygl/base.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/mygl/camera.cpp" "CMakeFiles/assignment_03.dir/src/mygl/camera.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/mygl/camera.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/mygl/mesh.cpp" "CMakeFiles/assignment_03.dir/src/mygl/mesh.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/mygl/mesh.cpp.o.d"
+  "/home/julian/uni/visual/PS/Assignment_3/src/mygl/shader.cpp" "CMakeFiles/assignment_03.dir/src/mygl/shader.cpp.o" "gcc" "CMakeFiles/assignment_03.dir/src/mygl/shader.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/julian/uni/visual/PS/Assignment_3/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/julian/uni/visual/PS/Assignment_3/external/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/julian/uni/visual/PS/Assignment_3/external/stb_image/CMakeFiles/stb_image.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/assignment_03.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment_03.dir/src/assignment_3.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/assignment_3.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/flag.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/flag.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/math/matrix3d.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/math/matrix3d.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/math/matrix4d.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/math/matrix4d.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/math/vector2d.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/math/vector2d.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/math/vector3d.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/math/vector3d.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/math/vector4d.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/math/vector4d.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/mygl/base.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/mygl/base.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/mygl/camera.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/mygl/camera.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/mygl/mesh.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/mygl/mesh.cpp.o.d"
+  "CMakeFiles/assignment_03.dir/src/mygl/shader.cpp.o"
+  "CMakeFiles/assignment_03.dir/src/mygl/shader.cpp.o.d"
+  "bin/assignment_03"
+  "bin/assignment_03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
